6a20e0339388f9e5364c1576363c42041d5984a1
tyler <tyler@hollands.net.nz>
2015-08-12 21:01:42 +1200

    updated views and routes so that some more magical jazz gets called upon compatible web browsers (and if SmartGit acknowledges the existence of .gitignore then all that stuff should be working aswell)

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', {});
+  res.render('index', {title: "Tyler Holland"});
 });
 
 module.exports = router;
